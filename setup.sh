22c2b44c0e5af60bc30edd25869206683862f7b0
Chris O'Neil <chris.oneil@gmail.com>
2017-09-15 10:52:38 +0100

    Ensure git is not installed at this stage
    If we're running in a corporate proxy environment, we install a
    customised version of git that uses the openssl library rather than
    gnutls, as this can cause weird issues with self signed SSL setups in
    corporate environments.

--- setup.sh
+++ setup.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
 apt-get update -y
-apt-get install -y curl git libffi-dev libssl-dev python-dev python-pip
+apt-get remove git -y
+apt-get install -y curl libffi-dev libssl-dev python-dev python-pip
 
-pip install --upgrade pip
-pip install --upgrade pyasn1 setuptools
-pip install ansible==2.1.1.0
+pip install --upgrade pip --cert /usr/local/share/ca-certificates/slc.crt
+pip install --upgrade pyasn1 setuptools --cert /usr/local/share/ca-certificates/slc.crt
+pip install ansible==2.1.1.0 --cert /usr/local/share/ca-certificates/slc.crt
