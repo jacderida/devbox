b088975a46399b062a2835ba0a1336eaa740ea98
Chris O'Neil <chris.oneil@gmail.com>
2017-10-28 12:52:27 +0100

    Use latest version of Ansible

--- sh/setup_debian.sh
+++ sh/setup_debian.sh
@@ -5,4 +5,4 @@ apt-get install -y curl git libffi-dev libssl-dev python-dev python-pip
 
 pip install --upgrade pip
 pip install --upgrade pyasn1 setuptools
-pip install ansible==2.1.1.0
+pip install ansible
