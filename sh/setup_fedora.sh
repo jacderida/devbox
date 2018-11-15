7f42a22973195cee2c4567b137ad983b825b825a
Chris O'Neil <chris.oneil@gmail.com>
2018-11-15 22:06:16 +0000

    fix/vm: prerequisites for get_url module

--- sh/setup_fedora.sh
+++ sh/setup_fedora.sh
@@ -2,6 +2,7 @@
 
 dnf update -y
 dnf install -y python python-dnf
+dnf install -y python3-libselinux python2-libselinux
 dnf install -y libffi-devel openssl-devel python-devel redhat-rpm-config
 
 pip install --upgrade pip
