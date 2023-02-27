03fbb35c26642d86a714a031a7c9bfe4751d9585
Chris O'Neil <chriso83@protonmail.com>
2023-02-27 22:32:05 +0000

    Remove Ansible
    There's simply no need to have Ansible.
    
    I think everything I need can be installed from `pacman`, `yay` and `cargo`, and my dotfiles has the
    configuration.

--- sh/setup_fedora.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-dnf update -y
-dnf install -y python python-dnf
-dnf install -y python3-libselinux python2-libselinux
-dnf install -y libffi-devel openssl-devel python-devel redhat-rpm-config
-
-pip install --upgrade pip
-pip install ansible
